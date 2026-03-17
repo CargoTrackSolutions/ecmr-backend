32be6d6c642d3cd0046cd790533c32c6139fe3ca
Zoltán Aranyi <aranyi.zoltan.po@gmail.com>
2026-03-17 12:04:40 +0200

    fix: add allowedHeaders to cors mapping

--- src/main/java/org/openlogisticsfoundation/ecmr/config/WebMvcConfig.java
+++ src/main/java/org/openlogisticsfoundation/ecmr/config/WebMvcConfig.java
@@ -25,7 +25,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins(this.corsOrigins.toArray(new String[0]))
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
-                 .allowedHeaders("*")
+                .allowedHeaders("*")
                 .allowCredentials(true);
     }
 }
